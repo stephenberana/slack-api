b8686e5783638c36b27c16f77415dbde3aa1c33c
stephenberana <stephenberana@gmail.com>
2021-08-29 09:29:29 +0800

    Pushes refactored code but still figuring out 404 bug

--- src/UserRegistration.js
+++ src/UserRegistration.js
@@ -1,51 +1,88 @@
 import axios from "axios";
-import React from "react";
-import { useForm } from "react-hook-form";
+import React, { useState } from "react";
 
-const UserRegistration = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
-    console.log(data);
+export default function UserRegistration(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [password_confirmation, setPasswordConfirmation] = useState("");
+
+  console.log(email, password, password_confirmation);
+
+  // const { register, handleSubmit } = useForm();
+  // const onSubmit = (data) => {
+  //   console.log(data);
+  // };
+
+  function onUserRegistration(e) {
+    e.preventDefault();
+    const postData = {
+      email,
+      password,
+      password_confirmation,
+    };
+
+    const config = {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    };
+
+    axios
+      .post({
+        url: "http://206.189.91.54/api/v1/auth",
+        data: postData,
+        headers: config,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  const passwordConfirmation = (password, password_confirmation) => {
+    if (password !== password_confirmation) {
+      alert("Please make sure that both passwords match.");
+      return false;
+    } else return true;
   };
 
-  axios({
-    method: "post",
-    url: "http://206.189.91.54/api/v1/auth/",
-    data: {
-      data: {
-        email: "",
-        password: "",
-        password_confirmation: "",
-      },
-    },
-  })
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={onUserRegistration}>
       <label>Email</label>
-      <input {...register("email")} placeholder="Input email" />
+      <input
+        type="email"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Input email"
+      />
       <br />
 
       <label>Password</label>
-      <input {...register("password")} placeholder="Input password" />
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Input password"
+      />
 
       <br />
 
       <label>Confirm Password</label>
       <input
-        {...register("password_confirmation")}
-        placeholder="Input password"
+        type="password"
+        name="password_confirmation"
+        value={password}
+        onChange={(e) => setPasswordConfirmation(e.target.value)}
+        placeholder="Confirm password"
       />
 
-      <button type="submit">Submit</button>
+      <button type="submit" onSubmit={() => passwordConfirmation}>
+        Submit
+      </button>
     </form>
   );
-};
-
-export default UserRegistration;
+}
